e2c9abb3d9db6fa13fe06e304cd081b4c8b8e7c0
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-08-27 19:11:11 +0200

    Chore: Added EntityFramework

---
+++ Data/UrlShrtDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlShrt.Data
+{
+    public class UrlShrtDbContext : DbContext
+    {
+        public UrlShrtDbContext(DbContextOptions<UrlShrtDbContext> options) : base(options) { }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using UrlShrt.Data;
 
 namespace UrlShrt
 {
@@ -24,6 +26,7 @@ namespace UrlShrt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UrlShrtDbContext>(option => option.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
             services.AddControllers();
         }
 
